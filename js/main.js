b9e7ccb6b1e46353f55c8daaabf99402c217c827
Conor McKenna <conorpmckenna@gmail.com>
2015-12-13 21:06:02 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 var loadingScreen = document.getElementById('loading-screen');
 var displayNone = 'display-none';
-
-
 // setTimeout(removeLoadingScreen, 5000);
 //  Checking that 'removeLoadingScreen' is working by using a setTimeout to immitate the page loading
 
-window.onload = removeLoadingScreen();
+// window.onload = removeLoadingScreen () {
+//   loadingScreen.classList.add(displayNone);
+// };
+
 
 function removeLoadingScreen () {
+  var topTitle = document.getElementById('title').innerHTML;
+  var loadedTitle = 'your page is now loaded';
+
   loadingScreen.classList.add(displayNone);
+  topTitle = 'Your page is loaded';
+
+
+
 }
+
+window.onload = removeLoadingScreen;
